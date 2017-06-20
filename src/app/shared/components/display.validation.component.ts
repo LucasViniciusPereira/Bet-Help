2ffea562a35e0e561a8cf1fc610c4193777cb4fb
Lucas Vinicius Pereira <lucasvp29@hotmail.com>
2017-06-19 21:20:26 -0300

    componente de validação

--- src/app/shared/components/display.validation.component.ts
+++ src/app/shared/components/display.validation.component.ts
@@ -4,7 +4,7 @@ import { DisplayValidationService } from '../services/display.validation.service
 
 @Component({
     selector: 'display-validation',
-    template: '<div for="text" data-error="wrong" data-success="right">{{errorMessage}}</div>'
+    template: '<small data-error="" data-success="" class="text-red">{{errorMessage}}</small>'
 })
 
 export class DisplayValidation {
@@ -15,8 +15,6 @@ export class DisplayValidation {
     get errorMessage(): string {
         for (let propertyName in this.control.errors) {
             if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
-                console.log(this.control.errors[propertyName]);
-
                 return this.svcValidation.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
             }
         }
