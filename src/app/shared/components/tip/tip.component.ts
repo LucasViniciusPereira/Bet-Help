5ed47074a82df96d16f8687d2fd595ce5492f5c7
Lucas Vinicius Pereira <lucasvp29@hotmail.com>
2017-07-14 22:09:45 -0300

    .

--- src/app/shared/components/tip/tip.component.ts
+++ src/app/shared/components/tip/tip.component.ts
@@ -1,4 +1,6 @@
+import { element } from 'protractor';
 import { Component, OnInit, Input } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-tip',
@@ -8,10 +10,20 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TipComponent implements OnInit {
   p = 1;
   @Input('tips') lstTips: Array<any>;
+  @Input() showControls = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  win(element: any) {
+    console.log(element);
+    $('#modalConfirmTip').modal('show');
+  }
+
+  lose(element: any) {
+    console.log(element);
   }
 
 }
